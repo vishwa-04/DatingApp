11b36dcb80154086e15c19efc75f517f85731d8a
vishwa-04 <vishwa.joshi.2023@gmail.com>
2023-11-06 22:56:45 +0530

    code remove

--- android/app/src/main/java/com/datingappproject/MainActivity.java
+++ android/app/src/main/java/com/datingappproject/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "DatingAppProject";
   }
-    @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+  //   @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(null);
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
