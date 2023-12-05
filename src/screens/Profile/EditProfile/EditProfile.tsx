4702d98bb7ae2d45574b458cd8d1f9bc17fc023b
Kaushal Panchal <92462707+kaushal91198@users.noreply.github.com>
2023-12-05 22:56:35 +0530

    working

--- src/screens/Profile/EditProfile/EditProfile.tsx
+++ src/screens/Profile/EditProfile/EditProfile.tsx
@@ -1,89 +1,122 @@
-import {View, Text, Image} from 'react-native';
-import React from 'react';
+import {View, Text, Image, ActivityIndicator} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {AllImages} from '@assets';
 import {useTailwind} from 'tailwind-rn';
 import {ProfileBackground} from '@components';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '@types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {asyncStorageConst} from '@constants';
 
 export const EditProfile = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList>) => {
   const tw = useTailwind();
 
+  const [userData, setUserData] = useState<any>(null);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    AsyncStorage.getItem(asyncStorageConst.loggedInUserData)
+      .then((data: any) => setUserData(JSON.parse(data)))
+      .catch(e => {
+        console.log(e);
+      })
+      .finally(() => {
+        setLoader(false);
+      });
+  }, []);
+
   return (
-    <ProfileBackground header={'Edit Profile'} navigate={navigation}>
-      <View>
-        <Text style={tw('text-[#6f6f6f] bg-[#fafafa] p-3')}>
-          Public Profile
-        </Text>
-        <View style={tw('flex-row justify-between items-center p-3')}>
-          <View style={tw('flex-row justify-between items-center gap-2')}>
-            <Image source={AllImages.FirstName} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              First Name
+    <>
+      {!loader ? (
+        <ProfileBackground header={'Edit Profile'} navigate={navigation}>
+          <View>
+            <Text style={tw('text-[#6f6f6f] bg-[#fafafa] p-3')}>
+              Public Profile
             </Text>
+            <View style={tw('flex-row justify-between items-center p-3')}>
+              <View style={tw('flex-row justify-between items-center gap-2')}>
+                <Image
+                  source={AllImages.FirstName}
+                  style={tw('object-cover')}
+                />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  First Name
+                </Text>
+              </View>
+              <Text style={tw('text-black')}>{userData.username}</Text>
+            </View>
+            <View style={tw('flex-row justify-between items-center p-3')}>
+              <View style={tw('flex-row justify-between items-center gap-2')}>
+                <Image
+                  source={AllImages.Nationality}
+                  style={tw('object-cover')}
+                />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Nationality
+                </Text>
+              </View>
+              <Text style={tw('text-black')}>{userData.nationality}</Text>
+            </View>
+            <View style={tw('flex-row justify-between p-3')}>
+              <View style={tw('flex-row justify-between items-center gap-2')}>
+                <Image source={AllImages.Emirates} style={tw('object-cover')} />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Emirates
+                </Text>
+              </View>
+              <Text style={tw('text-black')}>{userData?.emirates || ''}</Text>
+            </View>
+            <View style={tw('flex-row justify-between p-3')}>
+              <View style={tw('flex-row justify-between items-center gap-2')}>
+                <Image source={AllImages.Age} style={tw('object-cover')} />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Age
+                </Text>
+              </View>
+              <Text style={tw('text-black')}>{userData.age}</Text>
+            </View>
+            <View style={tw('flex-row justify-between p-3')}>
+              <View style={tw('flex-row justify-between items-center gap-2')}>
+                <Image
+                  source={AllImages.Occupation}
+                  style={tw('object-cover')}
+                />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Occupation
+                </Text>
+              </View>
+              <Text style={tw('text-black')}>{userData.occupation}</Text>
+            </View>
           </View>
-          <Text style={tw('text-black')}>bsdjbfsd</Text>
-        </View>
-        <View style={tw('flex-row justify-between items-center p-3')}>
-          <View style={tw('flex-row justify-between items-center gap-2')}>
-            <Image source={AllImages.Nationality} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Nationality
+          <View>
+            <Text style={tw('text-[#6f6f6f] bg-[#fafafa] p-3')}>
+              Private Detail
             </Text>
+            <View style={tw('flex-row justify-between p-3')}>
+              <View style={tw('flex-row justify-between items-center gap-2')}>
+                <Image source={AllImages.Email} style={tw('object-cover')} />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Email
+                </Text>
+              </View>
+              <Text style={tw('text-black')}>{userData?.email || ''}</Text>
+            </View>
+            <View style={tw('flex-row justify-between p-3')}>
+              <View style={tw('flex-row justify-between items-center gap-2')}>
+                <Image source={AllImages.Phone} style={tw('object-cover')} />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Phone
+                </Text>
+              </View>
+              <Text style={tw('text-black')}>{userData?.contact || ''}</Text>
+            </View>
           </View>
-          <Text style={tw('text-black')}>bsdjbfsd</Text>
-        </View>
-        <View style={tw('flex-row justify-between p-3')}>
-          <View style={tw('flex-row justify-between items-center gap-2')}>
-            <Image source={AllImages.Emirates} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Emirates
-            </Text>
-          </View>
-          <Text style={tw('text-black')}>bsdjbfsd</Text>
-        </View>
-        <View style={tw('flex-row justify-between p-3')}>
-          <View style={tw('flex-row justify-between items-center gap-2')}>
-            <Image source={AllImages.Age} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>Age</Text>
-          </View>
-          <Text style={tw('text-black')}>bsdjbfsd</Text>
-        </View>
-        <View style={tw('flex-row justify-between p-3')}>
-          <View style={tw('flex-row justify-between items-center gap-2')}>
-            <Image source={AllImages.Occupation} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Occupation
-            </Text>
-          </View>
-          <Text style={tw('text-black')}>bsdjbfsd</Text>
-        </View>
-      </View>
-      <View>
-        <Text style={tw('text-[#6f6f6f] bg-[#fafafa] p-3')}>
-          Private Detail
-        </Text>
-        <View style={tw('flex-row justify-between p-3')}>
-          <View style={tw('flex-row justify-between items-center gap-2')}>
-            <Image source={AllImages.Email} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Email
-            </Text>
-          </View>
-          <Text style={tw('text-black')}>bsdjbfsd</Text>
-        </View>
-        <View style={tw('flex-row justify-between p-3')}>
-          <View style={tw('flex-row justify-between items-center gap-2')}>
-            <Image source={AllImages.Phone} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Phone
-            </Text>
-          </View>
-          <Text style={tw('text-black')}>bsdjbfsd</Text>
-        </View>
-      </View>
-    </ProfileBackground>
+        </ProfileBackground>
+      ) : (
+        <ActivityIndicator size={'large'} color={'#D38CDF'} />
+      )}
+    </>
   );
 };
--- src/screens/Profile/UserProfile/UserProfile.tsx
+++ src/screens/Profile/UserProfile/UserProfile.tsx
@@ -5,8 +5,9 @@ import {
   TouchableOpacity,
   Image,
   Pressable,
+  ActivityIndicator,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {AllImages} from '@assets';
 import {useTailwind} from 'tailwind-rn';
 import {ButtonLoader, HorizontalLine} from '@components';
@@ -23,6 +24,19 @@ export const UserProfile = ({
 }: NativeStackScreenProps<RootStackUserList>) => {
   const tw = useTailwind();
   const [logoutLoader, setLogoutLoader] = useState(false);
+  const [userData, setUserData] = useState<any>(null);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    AsyncStorage.getItem(asyncStorageConst.loggedInUserData)
+      .then((data: any) => setUserData(JSON.parse(data)))
+      .catch(e => {
+        console.log(e);
+      })
+      .finally(() => {
+        setLoader(false);
+      });
+  }, []);
 
   const logoutFunc = async () => {
     try {
@@ -46,83 +60,102 @@ export const UserProfile = ({
   };
 
   return (
-    <ImageBackground
-      source={AllImages.AppBg}
-      style={tw('h-full w-full relative')}>
-      <TouchableOpacity
-        style={tw('absolute top-5 mx-3')}
-        onPress={() => navigation.goBack()}>
-        <Image source={AllImages.LeftArrow} style={tw('object-cover')} />
-      </TouchableOpacity>
-      <View
-        style={tw('absolute top-1/4 rounded-t-3xl h-3/4 bg-white w-full p-5')}>
-        <View style={tw('mt-28')}>
-          <Pressable
-            style={tw('flex-row justify-start items-center gap-3 my-2')}
-            onPress={() => navigation.navigate('EditProfile')}>
-            <Image
-              source={AllImages.AccountDetail}
-              style={tw('object-cover')}
-            />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Account Detail
-            </Text>
-          </Pressable>
-          <HorizontalLine />
-          <Pressable
-            style={tw('flex-row justify-start items-center gap-3 my-2')}
-            onPress={() => navigation.navigate('Settings')}>
-            <Image source={AllImages.Setting} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Settings
-            </Text>
-          </Pressable>
-          <HorizontalLine />
-          <View style={tw('flex-row justify-start items-center gap-3 my-2')}>
-            <Image source={AllImages.ConatctUs} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              ConatctUs
-            </Text>
-          </View>
-          <HorizontalLine />
-          <View style={tw('flex-row justify-start items-center gap-3 my-2')}>
-            <Image source={AllImages.BlockedUsers} style={tw('object-cover')} />
-            <Text style={tw('font-medium text-base text-[#161616]')}>
-              Blocked Users
-            </Text>
-          </View>
-          <HorizontalLine />
-        </View>
-      </View>
-      <View style={tw('flex-1 justify-end py-16 px-3')}>
-        <View style={tw('gap-3 ')}>
+    <>
+      {!loader ? (
+        <ImageBackground
+          source={AllImages.AppBg}
+          style={tw('h-full w-full relative')}>
           <TouchableOpacity
-            style={tw('py-3 bg-[#4B164C] rounded-3xl font-semibold text-base')}
-            disabled={logoutLoader}
-            onPress={() => logoutFunc()}>
-            {logoutLoader ? (
-              <ButtonLoader />
-            ) : (
-              <Text style={tw('text-white text-center')}>Log out</Text>
-            )}
+            style={tw('absolute top-5 mx-3')}
+            onPress={() => navigation.goBack()}>
+            <Image source={AllImages.LeftArrow} style={tw('object-cover')} />
           </TouchableOpacity>
-          <Text style={tw('text-[#4B164C] text-center font-medium text-sm')}>
-            Delete Account
-          </Text>
-          <TouchableOpacity />
-        </View>
-      </View>
-      <View style={tw('absolute w-full top-24')}>
-        <View style={tw('flex-col justify-center items-center')}>
-          <Image
-            source={AllImages.UserPhoto}
-            style={tw('h-40 w-40 object-cover rounded-full')}
-          />
-          <Text style={tw('font-medium text-sm text-black')}>
-            Jesica arnold
-          </Text>
-        </View>
-      </View>
-    </ImageBackground>
+          <View
+            style={tw(
+              'absolute top-1/4 rounded-t-3xl h-3/4 bg-white w-full p-5',
+            )}>
+            <View style={tw('mt-28')}>
+              <Pressable
+                style={tw('flex-row justify-start items-center gap-3 my-2')}
+                onPress={() => navigation.navigate('EditProfile')}>
+                <Image
+                  source={AllImages.AccountDetail}
+                  style={tw('object-cover')}
+                />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Account Detail
+                </Text>
+              </Pressable>
+              <HorizontalLine />
+              <Pressable
+                style={tw('flex-row justify-start items-center gap-3 my-2')}
+                onPress={() => navigation.navigate('Settings')}>
+                <Image source={AllImages.Setting} style={tw('object-cover')} />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Settings
+                </Text>
+              </Pressable>
+              <HorizontalLine />
+              <View
+                style={tw('flex-row justify-start items-center gap-3 my-2')}>
+                <Image
+                  source={AllImages.ConatctUs}
+                  style={tw('object-cover')}
+                />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  ConatctUs
+                </Text>
+              </View>
+              <HorizontalLine />
+              <View
+                style={tw('flex-row justify-start items-center gap-3 my-2')}>
+                <Image
+                  source={AllImages.BlockedUsers}
+                  style={tw('object-cover')}
+                />
+                <Text style={tw('font-medium text-base text-[#161616]')}>
+                  Blocked Users
+                </Text>
+              </View>
+              <HorizontalLine />
+            </View>
+          </View>
+          <View style={tw('flex-1 justify-end py-16 px-3')}>
+            <View style={tw('gap-3 ')}>
+              <TouchableOpacity
+                style={tw(
+                  'py-3 bg-[#4B164C] rounded-3xl font-semibold text-base',
+                )}
+                disabled={logoutLoader}
+                onPress={() => logoutFunc()}>
+                {logoutLoader ? (
+                  <ButtonLoader />
+                ) : (
+                  <Text style={tw('text-white text-center')}>Log out</Text>
+                )}
+              </TouchableOpacity>
+              <Text
+                style={tw('text-[#4B164C] text-center font-medium text-sm')}>
+                Delete Account
+              </Text>
+              <TouchableOpacity />
+            </View>
+          </View>
+          <View style={tw('absolute w-full top-24')}>
+            <View style={tw('flex-col justify-center items-center')}>
+              <Image
+                source={AllImages.UserPhoto}
+                style={tw('h-40 w-40 object-cover rounded-full')}
+              />
+              <Text style={tw('font-medium text-sm text-black')}>
+                {userData?.username}
+              </Text>
+            </View>
+          </View>
+        </ImageBackground>
+      ) : (
+        <ActivityIndicator size={'large'} color={'#D38CDF'} />
+      )}
+    </>
   );
 };
